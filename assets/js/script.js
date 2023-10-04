aa723ade570ff8891c5f5de3ca5284652c8e4b95
kittykatt210 <keperry30@icloud.com>
2023-10-03 22:12:36 -0500

    Added functionality to the start/home buttons; updated page titles; added js link to index.html page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,80 @@
+var startBtn = document.getElementById('generate1');
 
-// Facts api call
-var limit = 3;
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/facts?limit=' + limit,
-    headers: { 'X-Api-Key': 'r0xgD0GtvJJrEoy6u9/jlg==nkpcvornpjuE0Na7'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
 
-// Historical facts api call
-var text = 'roman empire'
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/historicalevents?text=' + text,
-    headers: { 'X-Api-Key': 'r0xgD0GtvJJrEoy6u9/jlg==nkpcvornpjuE0Na7'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
+function getFacts() {
+    // Facts api call
+    var limit = document.getElementById('amountInput').value;
 
-// Jokes api call
-var limit = 3
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/jokes?limit=' + limit,
-    headers: { 'X-Api-Key': 'DB4FieOP94WOQIx2LOBzoA==mCMWVRWRDZLdKPcD'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/facts?limit=' + limit,
+        headers: { 'X-Api-Key': 'r0xgD0GtvJJrEoy6u9/jlg==nkpcvornpjuE0Na7'},
+        contentType: 'application/json',
+        success: function(result) {
+            var fact = result[0].fact;
+            document.getElementById('randomFact').innerHTML = fact;
 
-// Dad jokes api call
-var limit = 3
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/dadjokes?limit=' + limit,
-    headers: { 'X-Api-Key': 'DB4FieOP94WOQIx2LOBzoA==mCMWVRWRDZLdKPcD'},
-    contentType: 'application/json',
-    success: function(result) {
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+
+
+    });
+}
+
+function getHistFacts() {
+    // Historical facts api call
+    var text = 'roman empire'
+
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/historicalevents?text=' + text,
+        headers: { 'X-Api-Key': 'r0xgD0GtvJJrEoy6u9/jlg==nkpcvornpjuE0Na7'},
+        contentType: 'application/json',
+        success: function(result) {
+            console.log(result);
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+    });
+}
+
+function getJokes() {
+    // Jokes api call
+    var limit = 3
+
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/jokes?limit=' + limit,
+        headers: { 'X-Api-Key': 'DB4FieOP94WOQIx2LOBzoA==mCMWVRWRDZLdKPcD'},
+        contentType: 'application/json',
+        success: function(result) {
+            console.log(result);
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+    });
+}
+
+function getDadJokes() {
+    // Dad jokes api call
+    var limit = 3
+
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/dadjokes?limit=' + limit,
+        headers: { 'X-Api-Key': 'DB4FieOP94WOQIx2LOBzoA==mCMWVRWRDZLdKPcD'},
+        contentType: 'application/json',
+        success: function(result) {
         console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
+        },
+        error: function ajaxError(jqXHR) {
         console.error('Error: ', jqXHR.responseText);
-    }
-});
+        }
+    });
+}
+
+startBtn.addEventListener('click', getFacts);
